acef942f67ce12642b0453e75e7c13a435f39899
Alexander Krivoschekov <alexandrsk@gmail.com>
2012-12-09 23:14:23 +0600

    Debianization

---
+++ tiny-weather-indicator/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tiny-weather-indicator',
+      version='0.0.1',
+      description='Tiny weather indicator for unity',
+      author='Alexandr Krivoschekov',
+      author_email='alexandrsk@gmail.com',
+      url='https://github.com/alrock/tiny-weather-indicator/',
+      scripts=['tiny-weather-indicator']
+     )
